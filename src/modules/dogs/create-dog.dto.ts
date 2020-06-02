1d349809ae17400e34f46f783f9dbdae12e5ea64
JHyeok <dev.jhyeok@gmail.com>
2020-06-02 18:56:02 +0900

    feat: Controllers 추가

---
+++ src/modules/dogs/create-dog.dto.ts
@@ -0,0 +1,5 @@
+export class CreateDogDto {
+  name: string;
+  age: number;
+  breed: string;
+}
--- src/modules/main/app.module.ts
+++ src/modules/main/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DogsController } from '../dogs/dogs.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, DogsController],
   providers: [AppService],
 })
 export class AppModule {}
