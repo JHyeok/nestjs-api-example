52257fe149692dd20d6d11d6211b4d4f6f42e16e
JHyeok <dev.jhyeok@gmail.com>
2020-06-03 19:11:38 +0900

    feat: Providers 추가

---
+++ src/modules/dogs/interfaces/dog.interface.ts
@@ -0,0 +1,5 @@
+export interface Dog {
+  name: string;
+  age: number;
+  breed: string;
+}
--- src/modules/main/app.module.ts
+++ src/modules/main/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DogsController } from '../dogs/dogs.controller';
+import { DogsService } from '../dogs/dogs.service';
 
 @Module({
   imports: [],
   controllers: [AppController, DogsController],
-  providers: [AppService],
+  providers: [AppService, DogsService],
 })
 export class AppModule {}
