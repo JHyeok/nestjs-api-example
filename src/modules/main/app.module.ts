a890cb8524beab8f885694cea533ce51ce947386
JHyeok <dev.jhyeok@gmail.com>
2020-06-04 15:02:05 +0900

    feat: Modules 추가

--- src/modules/main/app.module.ts
+++ src/modules/main/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DogsController } from '../dogs/dogs.controller';
-import { DogsService } from '../dogs/dogs.service';
+import { DogsModule } from '../dogs/dogs.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, DogsController],
-  providers: [AppService, DogsService],
+  imports: [DogsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
