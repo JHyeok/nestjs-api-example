d502274bd7f036227ce0201e894c1b266719e367
JHyeok <dev.jhyeok@gmail.com>
2020-06-07 19:29:52 +0900

    feat: Interceptors 추가

--- src/modules/main/app.module.ts
+++ src/modules/main/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DogsModule } from '../dogs/dogs.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from 'src/common/interceptors/logging.interceptor';
 
 @Module({
   imports: [DogsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // Interceptor 모든 모듈에 적용
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
