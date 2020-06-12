2843a4c2a7511927b1c222d0d65f2e73c083384f
JHyeok <dev.jhyeok@gmail.com>
2020-06-12 15:53:19 +0900

    feat: TypeORM을 ConfigModule 이용해서 데이터베이스 연결

--- src/modules/main/app.module.ts
+++ src/modules/main/app.module.ts
@@ -5,11 +5,32 @@ import { UserHttpModule } from '../users/users-http.module'
 import { DogsModule } from '../dogs/dogs.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggingInterceptor } from 'src/common/interceptors/logging.interceptor';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
+import { ConfigModule, ConfigService } from './../config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserHttpModule, DogsModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: configService.get('DB_TYPE'),
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          entities: [__dirname + './../**/**.entity{.ts,.js}'],
+          synchronize: configService.isEnv('dev'),
+        } as TypeOrmModuleAsyncOptions;
+      },
+    }),
+    ConfigModule,
+    UserHttpModule,
+    DogsModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
