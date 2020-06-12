bebb85fa7d4b738ed0831e24ac1cac49fa2ae0da
JHyeok <dev.jhyeok@gmail.com>
2020-06-12 15:32:30 +0900

    feat: TypeORM 연결

---
+++ src/modules/users/users-http.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users.module';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+
+@Module({
+  imports: [UsersModule],
+  providers: [UsersService],
+  controllers: [UsersController]
+})
+export class UserHttpModule {}
---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm'
+import { User } from './user.entity'
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    this.userRepository.save(createUserDto);
+  }
+
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  findOne(id: string): Promise<User> {
+    return this.userRepository.findOne(id);
+  }
+
+  async remove(id: string): Promise<void> {
+    await this.userRepository.delete(id);
+  }
+}
