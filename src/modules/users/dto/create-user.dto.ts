5475de1e7f775d1a59a94efdcea3e64c726218f8
JHyeok <dev.jhyeok@gmail.com>
2020-07-04 14:49:17 +0900

    feat: Swagger 적용

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,9 +1,12 @@
 import { IsString } from 'class-validator'
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @IsString()
+  @ApiPropertyOptional()
   firstName: string;
 
   @IsString()
+  @ApiPropertyOptional()
   lastName: string;
 }
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -2,28 +2,34 @@ import { Controller, Get, Post, HttpCode, Param, Body, Delete } from '@nestjs/co
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiTags('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get()
+  @ApiOperation({ description: '모든 User 조회' })
   async findAll(): Promise<User[]> {
     return this.usersService.findAll();
   }
 
   @Post()
   @HttpCode(201)
+  @ApiOperation({ description: 'User 생성' })
   async create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get(':id')
+  @ApiOperation({ description: 'Id가 일치하는 User 정보 조회' })
   async findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
   @Delete(':id')
+  @ApiOperation({ description: 'User 삭제' })
   async remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
