075164026f151d6b26bcba16a58655b0ac8e2bfb
JHyeok <dev.jhyeok@gmail.com>
2020-07-16 16:22:56 +0900

    refactor: 모듈 리팩토링

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -6,11 +6,13 @@ import {
   Param,
   Body,
   Delete,
+  Put,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 @ApiTags('users')
@@ -20,25 +22,31 @@ export class UserController {
   @Get()
   @ApiOperation({ description: '모든 User 조회' })
   async findAll(): Promise<User[]> {
-    return await this.userService.findAll();
+    return await this.userService.getUsers();
   }
 
   @Post()
   @HttpCode(201)
   @ApiOperation({ description: 'User 생성' })
   async create(@Body() createUserDto: CreateUserDto) {
-    return await this.userService.create(createUserDto);
+    return await this.userService.createUser(createUserDto);
   }
 
   @Get(':id')
   @ApiOperation({ description: 'Id가 일치하는 User 정보 조회' })
   async findOne(@Param('id') id: string) {
-    return await this.userService.findOne(id);
+    return await this.userService.getUserById(id);
+  }
+
+  @Put(':id')
+  @ApiOperation({ description: 'User 정보 수정' })
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return await this.userService.updateUser(id, updateUserDto);
   }
 
   @Delete(':id')
   @ApiOperation({ description: 'User 삭제' })
   async remove(@Param('id') id: string) {
-    return await this.userService.remove(id);
+    return await this.userService.removeUser(id);
   }
 }
