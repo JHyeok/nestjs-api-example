2649d4d1ef75618c7a17f3e47fb1c1c5b30e2fbd
JHyeok <dev.jhyeok@gmail.com>
2020-10-01 21:53:43 +0900

    refactor: 단위 테스트에 사용되는 메서드 추가
    엔티티에서 단위 테스트에 사용되는 Fake Repository 추가
    of 메서드 추가

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -3,14 +3,32 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
-  id: string;
+  public id: string;
 
   @Column()
-  firstName: string;
+  public firstName: string;
 
   @Column()
-  lastName: string;
+  public lastName: string;
 
   @Column({ default: true })
-  isActive: boolean;
+  public isActive: boolean;
+
+  public static of(params: Partial<User>): User {
+    const user = new User();
+
+    Object.assign(user, params);
+
+    return user;
+  }
+}
+
+/**
+ * 단위 테스트에 사용되는 Fake Repository
+ */
+export class UserRepositoryFake {
+  public async save(): Promise<void> {}
+  public async findOne(): Promise<void> {}
+  public async find(): Promise<void> {}
+  public async delete(): Promise<void> {}
 }
