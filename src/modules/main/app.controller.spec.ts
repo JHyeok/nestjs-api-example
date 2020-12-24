968b486b16899e7e1ad2416f745327fd96895ea9
JaeHyeok Kim <dev.jhyeok@gmail.com>
2020-12-24 23:12:07 +0900

    feat: Health Check API 추가
    ECS에서 Health Check 용도로 사용할 API 추가

--- src/modules/main/app.controller.spec.ts
+++ src/modules/main/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "OK"', () => {
+      expect(appController.healthCheck()).toBe('OK');
     });
   });
 });
