e3c1239f887dca168d7d7d46619da28565ed38bf
JaeHyeok Kim <dev.jhyeok@gmail.com>
2021-03-06 20:19:15 +0900

    chore: tsconfig.json 수정으로 인한 변경
    strict를 true로 해서 엄격한 검사를 한다.
    skipLibCheck는 d.ts 리소스가 부족하기 때문에 true로 설정한다.
    esModuleInterop를 사용해서 es6 모듈 사양에 따라 CommonJS 모듈을 가져올 수 있도록 수정한다.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/main/app.module';
+import helmet from 'helmet';
 import { setupSwagger } from './utils/swagger';
-import * as helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from './modules/config';
 
--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -6,11 +6,11 @@ export class CreateUserDto {
   @IsString({ message: '이름(firstName)의 형식이 올바르지 않습니다.' })
   @Length(1, 50)
   @ApiPropertyOptional()
-  firstName: string;
+  firstName!: string;
 
   @IsNotEmpty({ message: '성(lastName)은 필수값입니다.' })
   @IsString({ message: '성(lastName)의 형식이 올바르지 않습니다.' })
   @Length(1, 50)
   @ApiPropertyOptional()
-  lastName: string;
+  lastName!: string;
 }
