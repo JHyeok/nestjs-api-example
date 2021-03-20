61a7b98d652992d05da1c7e24ce3aea8ed69d0aa
JaeHyeok Kim <dev.jhyeok@gmail.com>
2021-03-20 11:15:41 +0900

    refactor: 프로젝트 구조 변경

--- ormconfig.ts
+++ ormconfig.ts
@@ -8,7 +8,7 @@ export default {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: ['src/modules/**/*.entity{.ts,.js}'],
+  entities: ['src/api/**/*.entity{.ts,.js}'],
   migrations: ['src/migrations/*{.ts,.js}'],
   cli: {
     migrationsDir: 'src/migrations',
--- src/decorator/post.decorator.ts
+++ src/decorator/post.decorator.ts
@@ -1,6 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-// Custom route decorators
+// Custom route decorator
 export const Car = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
