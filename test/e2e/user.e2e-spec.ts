58009a64e9d3ac1e60bbfb9db793f36c91d765d4
JaeHyeok Kim <dev.jhyeok@gmail.com>
2021-12-12 18:38:30 +0900

    test: 유저 E2E 테스트 수정

--- test/e2e/user.e2e-spec.ts
+++ test/e2e/user.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { getConnection } from 'typeorm';
 import { UserRepository } from 'src/api/user/user.repository';
 import { TypeOrmConfigService } from 'src/database/ormconfig.service';
 import { AppModule } from 'src/app.module';
@@ -37,33 +38,59 @@ describe('UserController (e2e)', () => {
     await app.init();
   });
 
+  beforeEach(async () => {
+    await userRepository.clear();
+  });
+
+  afterAll(async () => {
+    await getConnection().close();
+  });
+
   describe('GET /v1/users', () => {
     it('모든 유저 목록이 반환된다', async () => {
       await userRepository.save([
-        { firstName: 'first-1', lastName: 'last-2' },
-        { firstName: 'first-2', lastName: 'last-2' },
+        { firstName: 'JaeHyeok', lastName: 'Kim' },
+        { firstName: '재혁', lastName: '김' },
       ]);
 
-      const { body } = await request(app.getHttpServer())
-        .get('/v1/users')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200);
+      const response = await request(app.getHttpServer()).get('/v1/users');
 
+      expect(response.status).toBe(200);
+      const { body } = response;
       expect(body).toEqual([
         {
           id: expect.any(Number),
-          firstName: 'first-1',
-          lastName: 'last-2',
+          firstName: 'JaeHyeok',
+          lastName: 'Kim',
           isActive: true,
         },
         {
           id: expect.any(Number),
-          firstName: 'first-2',
-          lastName: 'last-2',
+          firstName: '재혁',
+          lastName: '김',
           isActive: true,
         },
       ]);
+    });
+  });
+
+  describe('POST /v1/users', () => {
+    it('유저를 생성한다', async () => {
+      const firstName = 'JaeHyeok';
+      const lastName = 'Kim';
+
+      const response = await request(app.getHttpServer())
+        .post('/v1/users')
+        .send({
+          firstName: firstName,
+          lastName: lastName,
+        });
+
+      expect(response.status).toBe(201);
+      const { body } = response;
+      expect(body.firstName).toBe(firstName);
+      expect(body.lastName).toBe(lastName);
+      expect(body.isActive).toBe(true);
     });
   });
 });
