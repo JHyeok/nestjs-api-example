53976b7f8d12fec87bde8475f4735c389e62de25
JaeHyeok Kim <dev.jhyeok@gmail.com>
2022-07-02 12:04:03 +0900

    feat: Exception filter 추가
    예외 필터를 추가해서 예외가 발생했을 때 응답을 적절하게 가공하도록 한다.

---
+++ src/filter/not-found-exception.filter.ts
@@ -0,0 +1,26 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  NotFoundException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { IExceptionResponse } from './exception.interface';
+
+@Catch(NotFoundException)
+export class NotFoundExceptionFilter implements ExceptionFilter {
+  catch(exception: NotFoundException, host: ArgumentsHost): void {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    const responseBody = exception.getResponse() as IExceptionResponse;
+
+    response.status(status).json({
+      statusCode: responseBody.statusCode,
+      message: responseBody.message,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+  }
+}
