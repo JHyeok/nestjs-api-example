7c2624198308757d29685f256f5683f5312a2ed9
JaeHyeok Kim <dev.jhyeok@gmail.com>
2022-08-07 17:05:59 +0900

    refactor: 유저를 생성하는 로직 리팩토링
    DTO에서 toEntity 메서드를 만들어서 사용하도록 수정한다.

--- src/api/user/dto/user-create-request.dto.ts
+++ src/api/user/dto/user-create-request.dto.ts
@@ -1,5 +1,6 @@
 import { IsNotEmpty, IsString, Length } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { User } from '../user.entity';
 
 export class UserCreateRequestDto {
   @IsNotEmpty({ message: '이름(firstName)은 필수값입니다.' })
@@ -13,4 +14,8 @@ export class UserCreateRequestDto {
   @Length(1, 50)
   @ApiProperty({ description: '성' })
   lastName: string;
+
+  toEntity(): User {
+    return User.create(this.firstName, this.lastName);
+  }
 }
