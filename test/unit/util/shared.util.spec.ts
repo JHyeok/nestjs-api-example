273f16a3acdc7c4179a38aa70c5fd724a3fdfffe
JaeHyeok Kim <dev.jhyeok@gmail.com>
2022-11-26 12:59:24 +0900

    test: shared.util.ts 단위 테스트 수정

--- test/unit/util/shared.util.spec.ts
+++ test/unit/util/shared.util.spec.ts
@@ -6,14 +6,17 @@ describe('SharedUtil', () => {
       expect(isEmpty(undefined)).toBe(true);
       expect(isEmpty(null)).toBe(true);
     });
+
     it('obj가 undefined이거나 null이 아닌 경우 false를 반환한다', () => {
       expect(isEmpty('3')).toBe(false);
     });
+
     it('배열이 비어 있거나 존재하지 않으면 true를 반환한다', () => {
       expect(isEmpty([])).toBe(true);
       expect(isEmpty(null)).toBe(true);
       expect(isEmpty(undefined)).toBe(true);
     });
+
     it('배열이 비어 있지 않으면 false를 반환한다', () => {
       expect(isEmpty([1, 2])).toBe(false);
     });
