308bd40624c3807bf03ed80ce17749711a8bec26
JaeHyeok Kim <dev.jhyeok@gmail.com>
2024-02-17 13:05:08 +0900

    test: shared.util.ts 테스트 케이스 추가

--- src/util/shared.util.ts
+++ src/util/shared.util.ts
@@ -5,7 +5,7 @@
  * @param obj - obj
  * @returns {boolean} - obj가 비어 있으면 true를 반환하고, 그렇지 않으면 false를 반환한다.
  */
-export const isEmpty = (obj: any) => {
+export const isEmpty = (obj: any): boolean => {
   return (
     [Object, Array].includes((obj || {}).constructor) &&
     !Object.entries(obj || {}).length
