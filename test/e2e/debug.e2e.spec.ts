295bec387dbfd26ca903e6311160e2eae732c319
JaeHyeok Kim <dev.jhyeok@gmail.com>
2025-02-23 23:50:00 +0900

    test: DebugController E2E 테스트 추가 및 jest-mock-extended 적용

---
+++ test/e2e/debug.e2e.spec.ts
@@ -0,0 +1,49 @@
+import { Test } from '@nestjs/testing';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { DebugController } from 'src/api/debug/debug.controller';
+import { DebugService } from 'src/api/debug/debug.service';
+import { type MockProxy, mock, mockReset } from 'jest-mock-extended';
+import { setupApp } from 'src/config/common';
+
+describe('DebugController (e2e)', () => {
+  let app: INestApplication;
+  const mockDebugService: MockProxy<DebugService> = mock<DebugService>();
+
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      controllers: [DebugController],
+      providers: [{ provide: DebugService, useValue: mockDebugService }],
+    }).compile();
+
+    app = module.createNestApplication();
+    setupApp(app);
+    await app.init();
+  });
+
+  beforeEach(() => {
+    mockReset(mockDebugService);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('GET /debug/pprof/profile', () => {
+    it('CPU 프로파일링을 시작하고 파일명을 응답한다', async () => {
+      // given
+      const fileName = 'cpu-profile-1740188574460.pb.gz';
+      const expectedResponse = `CPU profiling completed. File saved: ${fileName}`;
+      mockDebugService.startCpuProfiling.mockResolvedValue(expectedResponse);
+
+      // when
+      const res = await request(app.getHttpServer()).get(
+        '/debug/pprof/profile?seconds=15',
+      );
+
+      // then
+      expect(res.status).toBe(HttpStatus.OK);
+      expect(res.text).toBe(expectedResponse);
+    });
+  });
+});
