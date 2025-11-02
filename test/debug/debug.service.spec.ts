09f04d71f92efba62fe100bfda4d281d5734c205
JaeHyeok Kim <dev.jhyeok@gmail.com>
2025-11-02 09:06:41 +0900

    test: DebugService 테스트 수정

--- test/debug/debug.service.spec.ts
+++ test/debug/debug.service.spec.ts
@@ -32,56 +32,68 @@ describe('DebugService', () => {
 
   describe('startCpuProfiling', () => {
     it('입력값이 없으면 기본값(30초)을 사용하여 프로파일링을 실행한다', async () => {
+      // given
       const mockProfile = {} as perftools.profiles.IProfile;
       jest.mocked(pprof.time.profile).mockResolvedValue(mockProfile);
       jest
         .mocked(pprof.encode)
         .mockResolvedValue(Buffer.from('mocked-profile'));
 
+      // when
       await debugService.startCpuProfiling();
 
+      // then
       expect(pprof.time.profile).toHaveBeenCalledWith({
         durationMillis: 30_000,
       });
     });
 
     it('입력된 초를 밀리초로 변환하여 프로파일링을 실행한다', async () => {
+      // given
       const mockProfile = {} as perftools.profiles.IProfile;
       jest.mocked(pprof.time.profile).mockResolvedValue(mockProfile);
       jest
         .mocked(pprof.encode)
         .mockResolvedValue(Buffer.from('mocked-profile'));
 
+      // when
       await debugService.startCpuProfiling('10');
 
+      // then
       expect(pprof.time.profile).toHaveBeenCalledWith({
         durationMillis: 10_000,
       });
     });
 
     it('잘못된 입력값이 들어오면 기본값(30초)으로 프로파일링을 실행한다', async () => {
+      // given
       const mockProfile = {} as perftools.profiles.IProfile;
       jest.mocked(pprof.time.profile).mockResolvedValue(mockProfile);
       jest
         .mocked(pprof.encode)
         .mockResolvedValue(Buffer.from('mocked-profile'));
 
+      // when
       await debugService.startCpuProfiling('abc');
 
+      // then
       expect(pprof.time.profile).toHaveBeenCalledWith({
         durationMillis: 30_000,
       });
     });
 
     it('CPU 프로파일링을 시작하고 파일 저장 후 성공 메시지를 반환한다', async () => {
+      // given
       const mockProfile = {} as perftools.profiles.IProfile;
       jest.mocked(pprof.time.profile).mockResolvedValue(mockProfile);
       jest
         .mocked(pprof.encode)
         .mockResolvedValue(Buffer.from('mocked-profile'));
 
+      // when
       const result = await debugService.startCpuProfiling('10');
 
+      // then
       expect(pprof.time.profile).toHaveBeenCalledWith({
         durationMillis: 10_000,
       });
@@ -91,13 +103,16 @@ describe('DebugService', () => {
     });
 
     it('CPU 프로파일링이 실패하면 예외를 던진다', async () => {
+      // given
       jest
         .mocked(pprof.time.profile)
         .mockRejectedValue(new Error('Profiling failed'));
 
-      await expect(debugService.startCpuProfiling('10')).rejects.toThrow(
-        'CPU profiling failed.',
-      );
+      // when
+      const result = () => debugService.startCpuProfiling('10');
+
+      // then
+      await expect(result).rejects.toThrow('CPU profiling failed.');
     });
   });
 });
